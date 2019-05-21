16e3a784d8f1357f2b9fec507a2f876691f0ca42
Franca <thiago.luis.e.franca@accenture.com>
2019-05-21 16:43:16 -0300

    Commit database

---
+++ treinamento_flutter/flutter_app/lib/detail.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/person.dart';
+import 'package:flutter_app/personDAO.dart';
+
+class DetailPage extends StatefulWidget {
+  Person person;
+
+  DetailPage({this.person});
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    final _controller = TextEditingController(text: widget.person?.name);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controller,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              if(widget.person == null){
+                widget.person = Person(name: _controller.text);
+              }else{
+                widget.person.name = _controller.text;
+              }
+              PersonDAO().insert(person: widget.person); 
+            },
+            child: Text("Save"),
+          )
+        ],
+      ),
+    );
+  }
+}
--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_app/list.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,53 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  SharedPreferences prefs;
-  final textFieldController = TextEditingController();
-
-  @override
-  void initState() async{
-    prefs = await SharedPreferences.getInstance();
-    super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: textFieldController,
-              ),
-            ),
-            RaisedButton(
-              onPressed: (){
-                prefs.setString("text", textFieldController.text);
-              },
-              child: Text("Save"),
-            )
-          ],
-        ),
-      ),
+      home: ListPage(),
     );
   }
 }
