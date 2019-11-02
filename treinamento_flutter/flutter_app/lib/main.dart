bf0519ad9a1830f4ef9036899fec76f9159537c5
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-02 10:46:50 -0300

    Commit slider

--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -25,38 +25,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  double value = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return Material(
+      child: Center(
+        child: Slider(
+          value: value,
+          onChanged: (value) {
+            setState(() {
+              this.value = value;
+            });
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
