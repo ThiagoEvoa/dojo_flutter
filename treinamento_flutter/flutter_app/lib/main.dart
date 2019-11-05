c33e183dd2d33fa2850b65de044667f16715918b
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-05 14:48:51 -0300

    Commit animated_icon

--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -53,11 +53,8 @@ class _MyHomePageState extends State<MyHomePage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+    return Material(
+      child: Center(
         child: IconButton(
           onPressed: () => _handleOnPressed(),
           iconSize: 50,
