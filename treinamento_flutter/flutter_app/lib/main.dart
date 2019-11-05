db45eeab6ddffcb421fb7efb616683036b8f7c42
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-05 13:56:12 -0300

    Commit geolocator

--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -60,9 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
