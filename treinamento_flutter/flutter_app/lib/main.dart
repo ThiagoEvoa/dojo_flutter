b6a28e229f6cf4716d9f7c810eeac75cf1f956c6
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-05 13:34:40 -0300

    Commit gradient

--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -29,9 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
