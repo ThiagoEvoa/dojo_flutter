05523cf30ce9d541af8125061c0e1c77ad24aecf
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-11 13:10:44 -0300

    Commit stack

--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -27,9 +27,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
+    return Material(
+      child: Center(
         child: Stack(
           children: <Widget>[
             Container(
