632dfa7b690f105048c584a890f5f36814eca1ab
Franca, Thiago Luis E <thiago.luis.e.franca@accenture.com>
2019-11-11 15:38:04 -0300

    Gradient

--- treinamento_flutter/flutter_app/lib/main.dart
+++ treinamento_flutter/flutter_app/lib/main.dart
@@ -28,9 +28,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
+    return Material(
+      child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
